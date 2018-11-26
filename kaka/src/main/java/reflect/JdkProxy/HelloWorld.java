135d1c5a92f741d455a11694acf12fc1b81f6670
konglinghui <1337860995@qq.com>
2018-11-26 20:15:04 +0800

    JDK代理case

---
+++ kaka/src/main/java/reflect/JdkProxy/HelloWorld.java
@@ -0,0 +1,10 @@
+package reflect.JdkProxy;
+
+/**
+ * @author konglinghui
+ * @date 2018/11/26
+ */
+
+public interface HelloWorld {
+    public void sayHello();
+}
---
+++ kaka/src/main/java/reflect/JdkProxy/HelloWorldImpl.java
@@ -0,0 +1,13 @@
+package reflect.JdkProxy;
+
+/**
+ * @author konglinghui
+ * @date 2018/11/26
+ */
+
+public class HelloWorldImpl implements HelloWorld{
+    @Override
+    public void sayHello(){
+        System.out.println("Hello World!");
+    }
+}

bf4898c8c9c7d6f57461efb9a964f8d74da26239
kongdada <1337860995@qq.com>
2019-05-27 00:20:05 +0800

    dubboClient

---
+++ dubbo/dubboClient/src/main/java/com/kong/App.java
@@ -0,0 +1,20 @@
+package com.kong;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("dubbo-consumer.xml");
+        context.start();
+        System.out.println("consumer start");
+        DemoApiService demoApiService = context.getBean(DemoApiService.class);
+        String result = demoApiService.getUser(1l);
+        System.out.println("result:" + result);
+
+        System.out.println(demoApiService.getUser(2l));
+        System.out.println(demoApiService.getUser(3l));
+    }
+}
